2a11a68ed0ee7e6eb37ab076f8c7ba242a29b33a
NickCrawford <ncrawfo7@kent.edu>
2016-08-27 12:34:17 -0400

    Project setup

---
+++ public/app.js
@@ -0,0 +1,256 @@
+;
+jQuery(function($){    
+    'use strict';
+
+/**
+     * All the code relevant to Socket.IO is collected in the IO namespace.
+     *
+     * @type {{init: Function, bindEvents: Function, onConnected: Function, onNewGameCreated: Function, playerJoinedRoom: Function, beginNewGame: Function, onNewWordData: Function, hostCheckAnswer: Function, gameOver: Function, error: Function}}
+     */
+    var IO = {
+
+        /**
+         * This is called when the page is displayed. It connects the Socket.IO client
+         * to the Socket.IO server
+         */
+        init: function() {
+            IO.socket = io.connect();
+            IO.bindEvents();
+        },
+
+        /**
+         * While connected, Socket.IO will listen to the following events emitted
+         * by the Socket.IO server, then run the appropriate function.
+         */
+        bindEvents : function() {
+            IO.socket.on('connected', IO.onConnected );
+            IO.socket.on('newGameCreated', IO.onNewGameCreated );
+            IO.socket.on('playerJoinedRoom', IO.playerJoinedRoom );
+
+            IO.socket.on('gameOver', IO.gameOver);
+            IO.socket.on('error', IO.error );
+        },
+
+         /**
+         * The client is successfully connected!
+         */
+        onConnected : function() {
+            // Cache a copy of the client's socket.IO session ID on the App
+            App.mySocketId = IO.socket.socket.sessionid;
+            // console.log(data.message);
+        },
+
+        /**
+         * A new game has been created and a random game ID has been generated.
+         * @param data {{ gameId: int, mySocketId: * }}
+         */
+        onNewGameCreated : function(data) {
+            App.Host.gameInit(data);
+        },
+
+        /**
+         * A player has successfully joined the game.
+         * @param data {{playerName: string, gameId: int, mySocketId: int}}
+         */
+        playerJoinedRoom : function(data) {
+            // When a player joins a room, do the updateWaitingScreen funciton.
+            // There are two versions of this function: one for the 'host' and
+            // another for the 'player'.
+            //
+            // So on the 'host' browser window, the App.Host.updateWiatingScreen function is called.
+            // And on the player's browser, App.Player.updateWaitingScreen is called.
+            App[App.myRole].updateWaitingScreen(data);
+        },
+
+        error : function(data) {
+            alert(data.message);
+        }
+
+    };
+
+    var App = {
+
+        /**
+         * Keep track of the gameId, which is identical to the ID
+         * of the Socket.IO Room used for the players and host to communicate
+         *
+         */
+        gameId: 0,
+
+        /**
+         * This is used to differentiate between 'Host' and 'Player' browsers.
+         */
+        myRole: '',   // 'Player' or 'Host'
+
+        /**
+         * The Socket.IO socket object identifier. This is unique for
+         * each player and host. It is generated when the browser initially
+         * connects to the server when the page loads for the first time.
+         */
+        mySocketId: '',
+
+        /* *************************************
+         *                Setup                *
+         * *********************************** */
+
+        /**
+         * This runs when the page initially loads.
+         */
+        init: function () {
+            App.cacheElements();
+            App.showInitScreen();
+            App.bindEvents();
+
+            // Initialize the fastclick library
+            FastClick.attach(document.body);
+        },
+
+        /**
+         * Create references to on-screen elements used throughout the game.
+         */
+        cacheElements: function () {
+            App.$doc = $(document);
+
+            // Templates
+            App.$gameArea = $('#gameArea');
+            App.$templateIntroScreen = $('#intro-screen-template').html();
+            App.$templateNewGame = $('#create-game-template').html();
+            App.$templateJoinGame = $('#join-game-template').html();
+            App.$hostGame = $('#host-game-template').html();
+        },
+
+        /**
+         * Create some click handlers for the various buttons that appear on-screen.
+         */
+        bindEvents: function () {
+            // Host
+            App.$doc.on('click', '#btnHostGame', App.Host.onHostClick);
+
+            // Player
+            App.$doc.on('click', '#btnJoinGame', App.Player.onJoinClick);
+            App.$doc.on('click', '#btnStart',App.Player.onPlayerStartClick);
+        },
+
+        /* *************************************
+         *             Game Logic              *
+         * *********************************** */
+
+        /**
+         * Show the initial Anagrammatix Title Screen
+         * (with Start and Join buttons)
+         */
+        showInitScreen: function() {
+            App.$gameArea.html(App.$templateIntroScreen);
+            App.doTextFit('.title');
+        },
+
+        Host : {
+        	/**
+             * Handler for the "HOST" button on the Title Screen.
+             */
+            onHostClick: function () {
+                console.log('Clicked "Create A Game"');
+                IO.socket.emit('hostCreateNewGame');
+            },
+
+
+        	/**
+             * The Host screen is displayed for the first time.
+             * @param data{{ gameId: int, mySocketId: * }}
+             */
+            gameInit: function (data) {
+                App.gameId = data.gameId;
+                App.mySocketId = data.mySocketId;
+                App.myRole = 'Host';
+
+                App.Host.displayNewGameScreen();
+                // console.log("Game started with ID: " + App.gameId + ' by host: ' + App.mySocketId);
+            },
+
+            /**
+             * Show the Host screen containing the game URL and unique game ID
+             */
+            displayNewGameScreen : function() {
+                // Fill the game screen with the appropriate HTML
+                App.$gameArea.html(App.$templateNewGame);
+
+                // Display the URL on screen
+                $('#gameURL').text(window.location.href);
+                App.doTextFit('#gameURL');
+
+                // Show the gameId / room id on screen
+                $('#spanNewGameCode').text(App.gameId);
+            }
+        },
+
+        /* *****************************
+           *        PLAYER CODE        *
+           ***************************** */
+
+        Player : {
+        	/**
+             * A reference to the socket ID of the Host
+             */
+            hostSocketId: '',
+
+            /**
+             * The player's name entered on the 'Join' screen.
+             */
+            myName: '',
+
+            /**
+             * Click handler for the 'JOIN' button
+             */
+            onJoinClick: function () {
+                // console.log('Clicked "Join A Game"');
+
+                // Display the Join Game HTML on the player's screen.
+                App.$gameArea.html(App.$templateJoinGame);
+            },
+
+            /**
+             * The player entered their name and gameId (hopefully)
+             * and clicked Start.
+             */
+            onPlayerStartClick: function() {
+                // console.log('Player clicked "Start"');
+
+                // collect data to send to the server
+                var data = {
+                    gameId : +($('#inputGameId').val()),
+                    playerName : $('#inputPlayerName').val() || 'anon'
+                };
+
+                // Send the gameId and playerName to the server
+                IO.socket.emit('playerJoinGame', data);
+
+                // Set the appropriate properties for the current player.
+                App.myRole = 'Player';
+                App.Player.myName = data.playerName;
+            },
+        },
+
+         /**
+         * Make the text inside the given element as big as possible
+         * See: https://github.com/STRML/textFit
+         *
+         * @param el The parent element of some text
+         */
+        doTextFit : function(el) {
+            textFit(
+                $(el)[0],
+                {
+                    alignHoriz:true,
+                    alignVert:false,
+                    widthOnly:true,
+                    reProcess:true,
+                    maxFontSize:300
+                }
+            );
+        }
+    };
+
+    IO.init();
+    App.init();
+
+}($));
